7d992461d2cbe188e0526fe4c43d36797add7bce
Nicolas Rabier <nicolas.rabier@gmail.com>
2020-07-10 22:06:22 +1000

    cleanup

--- src/main/java/com/kreinto/toolbox/scrapper/ManageWpScrapper.java
+++ src/main/java/com/kreinto/toolbox/scrapper/ManageWpScrapper.java
@@ -9,14 +9,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -39,18 +37,19 @@ public class ManageWpScrapper {
     public static final String MWP_SITE_STATUS_ICON_SPAN = "//mwp-site-status-icon//span";
     public static final String I_ANALYTICS_EVENT_OPEN_SINGLE_SITE = "//i[@analytics-event='Open Single Site']";
     public static final String SPAN_CONTAINS_CLASS_USER_NAME = "//span[contains(@class,'user-name')]";
+    public static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
 
     public ManageWpScrapper() {
         Properties props = FileUtil.loadPropertiesFromResources("my.properties");
         final String serverUrl = props.getProperty(SERVER_PATH);
         try {
-            System.setProperty("webdriver.chrome.driver", "/Users/nic/kreinto/projects/toolbox/drivers/macos/chromedriver");
+            System.setProperty(WEBDRIVER_CHROME_DRIVER, props.getProperty(WEBDRIVER_CHROME_DRIVER));
 
             final ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
             options.addArguments("window-size=1200x600");
 
-            WebDriver driver = new ChromeDriver(options);
+            final WebDriver driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
             driver.get(String.format("%s%s", serverUrl, LOGIN_URL));
 
@@ -61,36 +60,19 @@ public class ManageWpScrapper {
                 driver.findElement(By.name("password")).sendKeys(props.getProperty(PASSWORD));
                 driver.findElement(By.id("sign-in-button")).click();
 
-                // test if properly logged in
-                WebElement usernameElement = driver.findElement(By.xpath(SPAN_CONTAINS_CLASS_USER_NAME));
-                if (usernameElement != null && props.getProperty(LOGIN).equals(usernameElement.getText().trim())) {
+                final WebElement usernameElement = driver.findElement(By.xpath(SPAN_CONTAINS_CLASS_USER_NAME));
+                if (props.getProperty(LOGIN).equals(usernameElement.getAttribute("uib-tooltip"))) {
                     // once logged in go to list of websites
                     driver.get(String.format("%s%s", serverUrl, OVERVIEW_URL));
 
                     log.debug("Test if refresh button is spinning.");
-                    WebElement refreshButton = driver.findElement(By.xpath(BUTTON_NG_CLICK_SYNC_SITES));
-                    if ("disabled".equals(refreshButton.getAttribute("disabled"))) {
-                        Wait wait = new FluentWait(driver).withTimeout(Duration.ofSeconds(280)).pollingEvery(Duration.ofSeconds(5)).ignoring(NoSuchElementException.class);
-                        refreshButton = (WebElement) wait.until(new Function<WebDriver, WebElement>() {
-                            public WebElement apply(WebDriver driver) {
-                                log.debug("Test again if refresh button is spinning.");
-                                WebElement refreshButton = driver.findElement(By.xpath(BUTTON_NG_CLICK_SYNC_SITES));
-                                if (refreshButton != null && "disabled".equals(refreshButton.getAttribute("disabled"))) {
-                                    return null;
-                                } else {
-                                    return refreshButton;
-                                }
-                            }
-                        });
+                    final WebElement refreshButton = driver.findElement(By.xpath(BUTTON_NG_CLICK_SYNC_SITES));
 
-                        // WebDriverWait wait2 = new WebDriverWait(driver, 280);
-                        // wait2.until(ExpectedConditions.elementToBeClickable(refreshButton));
-                    }
+                    WebDriverWait wait = new WebDriverWait(driver, 600);
+                    ExpectedCondition<Boolean> syncingIsOver = arg0 -> !"disabled".equals(refreshButton.getAttribute("disabled"));
+                    wait.until(syncingIsOver);
 
-                    //TODO:
-                    // - grab the list of websites
-                    // - go to each website overview page
-                    // - click on 'update all'
+
                     List<WebElement> websites = driver.findElements(By.xpath(I_ANALYTICS_EVENT_OPEN_SINGLE_SITE));
                     List<String> websiteDashboardUrls = websites.stream().map(we -> we.getAttribute("href")).collect(Collectors.toList());
 
@@ -102,6 +84,9 @@ public class ManageWpScrapper {
                         log.info(String.format("site name: %s", siteName.getText()));
                         WebElement siteStatus = driver.findElement(By.xpath(MWP_SITE_STATUS_ICON_SPAN));
                         log.info(String.format("site status: %s", siteStatus.getAttribute("uib-tooltip")));
+                        WebElement siteNotes = driver.findElement(By.xpath("//textarea[@ng-model='currentNote']"));
+                        log.info(String.format("site notes: %s", siteNotes.getText()));
+
                         if (!siteStatus.getAttribute("class").contains("status-ok")) {
                             try {
                                 WebElement updateAllButton = driver.findElement(By.xpath(DIV_NG_CLICK_UPDATE_ALL_$_EVENT));
@@ -119,6 +104,7 @@ public class ManageWpScrapper {
                     }
                 }
             }
+
         } catch (Exception e) {
             log.error(ExceptionUtil.format(e));
         }
